88cb5517d6db4f25290d21f5555a9c65a86c469b
Ivang71 <artemtolv@gmail.com>
2024-01-07 11:03:54 +0500

    fix avatars display

--- components/recent-sales.tsx
+++ components/recent-sales.tsx
@@ -5,7 +5,7 @@ export function RecentSales() {
     <div className="space-y-8">
       <div className="flex items-center">
         <Avatar className="h-9 w-9">
-          <AvatarImage src="/avatars/01.png" alt="Avatar" />
+          <AvatarImage src="https://www.architecturalrecord.com/ext/resources/Contact/thumb/bridget-cogley-square-portrait.jpg?1616699583" alt="Avatar" />
           <AvatarFallback>OM</AvatarFallback>
         </Avatar>
         <div className="ml-4 space-y-1">
@@ -18,18 +18,18 @@ export function RecentSales() {
       </div>
       <div className="flex items-center">
         <Avatar className="flex h-9 w-9 items-center justify-center space-y-0 border">
-          <AvatarImage src="/avatars/02.png" alt="Avatar" />
+          <AvatarImage src="https://github.com/shadcn.png" alt="Avatar" />
           <AvatarFallback>JL</AvatarFallback>
         </Avatar>
         <div className="ml-4 space-y-1">
-          <p className="text-sm font-medium leading-none">Jackson Lee</p>
-          <p className="text-sm text-muted-foreground">jackson.lee@email.com</p>
+          <p className="text-sm font-medium leading-none">Morty Smith</p>
+          <p className="text-sm text-muted-foreground">morty.smith@email.com</p>
         </div>
         <div className="ml-auto font-medium">+$39.00</div>
       </div>
       <div className="flex items-center">
         <Avatar className="h-9 w-9">
-          <AvatarImage src="/avatars/03.png" alt="Avatar" />
+          <AvatarImage src="https://static5.lenskart.com/media/catalog/product/pro/1/thumbnail/480x480/9df78eab33525d08d6e5fb8d27136e95//model/l/i/blue-block-phone-&-computer-glasses:-black-full-rim-square-lenskart-air-signia-la-e13069-c1_7_dec_model_shoot188779_137974_13_dec_2022.jpg" alt="Avatar" />
           <AvatarFallback>IN</AvatarFallback>
         </Avatar>
         <div className="ml-4 space-y-1">
@@ -42,7 +42,7 @@ export function RecentSales() {
       </div>
       <div className="flex items-center">
         <Avatar className="h-9 w-9">
-          <AvatarImage src="/avatars/04.png" alt="Avatar" />
+          <AvatarImage src="https://cdn.openart.ai/stable_diffusion/b44dfdae5688c37e1bcfe2c46a7fbeb3b66f84ee_2000x2000.webp" alt="Avatar" />
           <AvatarFallback>WK</AvatarFallback>
         </Avatar>
         <div className="ml-4 space-y-1">
@@ -53,12 +53,12 @@ export function RecentSales() {
       </div>
       <div className="flex items-center">
         <Avatar className="h-9 w-9">
-          <AvatarImage src="/avatars/05.png" alt="Avatar" />
+          <AvatarImage src="https://play-lh.googleusercontent.com/KgaJ5zSDE-kCvl2oE1TY2JngtsBDRhTKlY6JCjtugfL2K5IMiW7Te3wSLYJBi9E89YUb" alt="Avatar" />
           <AvatarFallback>SD</AvatarFallback>
         </Avatar>
         <div className="ml-4 space-y-1">
-          <p className="text-sm font-medium leading-none">Sofia Davis</p>
-          <p className="text-sm text-muted-foreground">sofia.davis@email.com</p>
+          <p className="text-sm font-medium leading-none">Walter White</p>
+          <p className="text-sm text-muted-foreground">walter.white@email.com</p>
         </div>
         <div className="ml-auto font-medium">+$39.00</div>
       </div>
